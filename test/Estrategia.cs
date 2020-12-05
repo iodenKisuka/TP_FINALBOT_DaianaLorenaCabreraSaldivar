be7071f2fbbf5472348f255300742baed51befce
iodenKisuka <iodenteken@hotmail.com>
2020-12-05 17:56:42 -0300

    Modificando uml

--- test/Estrategia.cs
+++ test/Estrategia.cs
@@ -10,19 +10,52 @@ namespace DeepSpace
 		
 		public String Consulta1( ArbolGeneral<Planeta> arbol)
 		{
-			return "Implementar";
+			string texto = "";
+			/*Calcula y retorna un texto con la distancia que existe entre la raíz y el nodo del árbol que es enviado como
+parámetro que contiene el planeta más cercano perteneciente al Bot."*/
+			//arbol.nivel(arbol);
+			
+			return "Implementar1";
 		}
 
 
 		public String Consulta2( ArbolGeneral<Planeta> arbol)
 		{
-			return "Implementar";
+			//return "Implementar2";
+			Cola<ArbolGeneral<Planeta>> q = new Cola<ArbolGeneral<Planeta>>();
+			q.encolar(arbol);
+			int nivel = 0;
+			String mensaje = "";
+			while (!q.esVacia())
+			{
+				//int elementos = q.cantElementos;
+				nivel++;
+				int cantidadPorNivel = 0;
+				//while (elementos-- > 0)
+				while(!q.esVacia())
+				{
+					ArbolGeneral<Planeta> nodoActual = q.desencolar();
+
+					if (nodoActual.getDatoRaiz().Poblacion() > 10)
+					{
+						cantidadPorNivel++;
+					}
+
+					foreach (ArbolGeneral<Planeta> hijo in nodoActual.getHijos())
+					{
+						q.encolar(hijo);
+					}
+				}
+				mensaje += "Nivel " + nivel + ": " + cantidadPorNivel + "\n";
+			}
+			return mensaje;
+
 		}
 
 
 		public String Consulta3( ArbolGeneral<Planeta> arbol)
 		{
-			return "Implementar";
+			return "Implementar3";
 		}
 		
 		public Movimiento CalcularMovimiento(ArbolGeneral<Planeta> arbol)
