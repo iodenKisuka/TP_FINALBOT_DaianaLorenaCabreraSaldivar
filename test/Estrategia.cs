6fb7b47454195cdd7c0c07eb8349ba8b9dc5ec7d
iodenKisuka <iodenteken@hotmail.com>
2020-12-06 18:28:17 -0300

    verificacion de funcionamiento de consultas
    complete los metodos consultas y verifique su correcto funcionamiento

--- test/Estrategia.cs
+++ test/Estrategia.cs
@@ -23,41 +23,89 @@ namespace DeepSpace
 
 		public String Consulta2( ArbolGeneral<Planeta> arbol)
 		{
-			//return "Implementar2";
-			Cola<ArbolGeneral<Planeta>> q = new Cola<ArbolGeneral<Planeta>>();
-			q.encolar(arbol);
-			int nivel = 0;
-			String mensaje = "";
-			while (!q.esVacia())
+			/*Calcula y retorna en un texto la cantidad de planetas que tienen 
+			 * población mayor a 10 en cada nivel del árbol que es enviado como parámetro*/
+
+			string texto = "";
+			int cant_nodoNivel;
+			int cant_PlanetaMayorA10;
+			int nivel = 1;
+			List<ArbolGeneral<Planeta>> Lista_hijos = arbol.getHijos();
+			List<ArbolGeneral<Planeta>> Siguiente_nivel_hijos;
+			do
 			{
-				//int elementos = q.cantElementos;
-				nivel++;
-				int cantidadPorNivel = 0;
-				//while (elementos-- > 0)
-				while(!q.esVacia())
+				cant_nodoNivel = Lista_hijos.Count;
+				cant_PlanetaMayorA10 = 0;
+				//lista vacia que tendra los nodos por nivel
+				Siguiente_nivel_hijos = new List<ArbolGeneral<Planeta>>();
+				foreach (ArbolGeneral<Planeta> Obtener_hijo in Lista_hijos)
 				{
-					ArbolGeneral<Planeta> nodoActual = q.desencolar();
 
-					if (nodoActual.getDatoRaiz().Poblacion() > 10)
+					if(Obtener_hijo.getDatoRaiz().Poblacion() > 10)
+                    {
+						cant_PlanetaMayorA10++;
+                    }
+					//obtengo los nodos hijos del nodo obtener_hijos y lo agrego a la lista de nodos que tiene siguiente nivel
+					foreach (ArbolGeneral<Planeta> hijo in Obtener_hijo.getHijos())
 					{
-						cantidadPorNivel++;
+						Siguiente_nivel_hijos.Add(hijo);
 					}
 
-					foreach (ArbolGeneral<Planeta> hijo in nodoActual.getHijos())
-					{
-						q.encolar(hijo);
-					}
 				}
-				mensaje += "Nivel " + nivel + ": " + cantidadPorNivel + "\n";
-			}
-			return mensaje;
 
+				//texto += "\nEl nivel " + nivel + " tiene : " + cant_PlanetaMayorA10 + " nodos con una población mayor a 10\n";
+				texto += "El nivel " + nivel + " tiene : " + cant_PlanetaMayorA10 + " nodos con una población mayor a 10\n";
+
+
+				Lista_hijos = Siguiente_nivel_hijos;
+				nivel++;
+			} while (Siguiente_nivel_hijos.Count > 0);
+
+
+
+			return texto+ "\n";
 		}
 
 
 		public String Consulta3( ArbolGeneral<Planeta> arbol)
 		{
-			return "Implementar3";
+			/*Calcula y retorna en un texto el promedio poblacional por nivel del árbol que es enviado como parámetro. */
+			string texto = "\n"+ "\n";
+			int cant_nodoNivel;
+			int cant_Poblacion;
+			int nivel = 1;
+			List<ArbolGeneral<Planeta>> Lista_hijos = arbol.getHijos();
+			List<ArbolGeneral<Planeta>> Siguiente_nivel_hijos;
+			do
+			{
+				cant_nodoNivel = Lista_hijos.Count;
+				cant_Poblacion = 0;
+				//lista vacia que tendra los nodos por nivel
+				Siguiente_nivel_hijos = new List<ArbolGeneral<Planeta>>();
+				foreach (ArbolGeneral<Planeta> Obtener_hijo in Lista_hijos)
+				{
+					
+					cant_Poblacion += Obtener_hijo.getDatoRaiz().Poblacion();
+					//obtengo los hijos del nodo obtener hijos y lo agrego a la lista de nodos que tiene siguiente nivel
+					foreach (ArbolGeneral<Planeta> hijo in Obtener_hijo.getHijos())
+					{
+						Siguiente_nivel_hijos.Add(hijo);
+					}
+					
+				}
+				int promedio = 0;
+				if (cant_nodoNivel > 0)
+				{
+					promedio = (cant_Poblacion / cant_nodoNivel);
+				}
+				texto += "El nivel " + nivel + " tiene en promedio poblicional : " + promedio; //+ "\n";
+				texto+= "\ncant. nodo " + cant_nodoNivel+ " cant. poblacion total "+ cant_Poblacion +"\n";
+
+				Lista_hijos = Siguiente_nivel_hijos;
+				nivel++;
+			} while (Siguiente_nivel_hijos.Count > 0);
+
+			return texto;
 		}
 		
 		public Movimiento CalcularMovimiento(ArbolGeneral<Planeta> arbol)
@@ -77,22 +125,24 @@ namespace DeepSpace
 			}
 			else
 			{
-				//-1 si no esta el dato
 				int nivel = 1;
-				List<ArbolGeneral<Planeta>> hijos = arbol.getHijos();
+				List<ArbolGeneral<Planeta>> Lista_hijos = arbol.getHijos();
 				List<ArbolGeneral<Planeta>> Siguiente_nivel_hijos;
 				do
 				{
+					//lista vacia que tendra los nodos por nivel
 					Siguiente_nivel_hijos = new List<ArbolGeneral<Planeta>>();
-					foreach (ArbolGeneral<Planeta> Obtener_hijo in hijos)
-					{
+
+					foreach (ArbolGeneral<Planeta> Obtener_hijo in Lista_hijos)
+					{	//cuando encuentro un nodo que es ia retorno el nivel
 						if (Obtener_hijo.getDatoRaiz().EsPlanetaDeLaIA()) { return nivel; }
+						//obtengo los hijos del nodo obtener hijos y lo agrego a la lista de nodos que tiene siguiente nivel
 						foreach (ArbolGeneral<Planeta> hijo in Obtener_hijo.getHijos())
 						{
 							Siguiente_nivel_hijos.Add(hijo);
 						}
 					}
-					hijos = Siguiente_nivel_hijos;
+					Lista_hijos = Siguiente_nivel_hijos;
 					nivel++;
 				} while (Siguiente_nivel_hijos.Count > 0);
 				//-1 si no esta el dato
